8bf77906eb572bd820328bc4c72dadf2c8ed9c37
Andie Saizan <asaizan@paylocity.com>
2017-07-28 00:14:39 -0500

    Async demo

---
+++ SimpleAsnycAwait/SimpleAsnycAwait/AsyncAwaitDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleAsyncAwait
+{
+  public class AsyncAwaitDemo
+  {
+    public Task StartCounter()
+    {
+      return Task.Run(async () => await DoStuff());
+
+    }
+    private async Task<string> DoStuff()
+    {
+      var result = await LongRunningOperation();
+      Console.WriteLine($"Counts complete: {result}");
+      return result;
+    }
+
+    private static Task<string> LongRunningOperation()
+    {
+      int counter;
+
+      for (counter = 0; counter < 10; counter++)
+        {
+        Thread.Sleep(3000);
+        Console.WriteLine(counter);
+        }
+
+      return Task.FromResult("Counter = " + counter);
+    }
+  }
+}
---
+++ SimpleAsnycAwait/SimpleAsnycAwait/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpleAsyncAwait
+{
+  class Program
+  {
+    static void Main()
+    {
+      var demo = new AsyncAwaitDemo();
+
+      var counterTask = demo.StartCounter();
+
+      while (true)
+      {
+        if (!counterTask.IsCompleted)
+        {
+          var input = Console.ReadLine();
+          Console.WriteLine($"Read {input} on the Main Thread While Counter Task is Running...................");
+        }
+        else
+        {
+          Console.WriteLine($"Doing Stuff on the Main Thread...................");
+        }
+      }
+    }
+  }
+}
